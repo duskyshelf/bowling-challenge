a1f506182181a8e02d34d45dfd7f77afa05e6b95
DuskyShelf <dupsdale@gmail.com>
2015-10-06 16:10:37 +0100

    small refactoring

--- spec/Bowlingspec.js
+++ spec/Bowlingspec.js
@@ -4,15 +4,17 @@ var scoresheet;
 
   describe('Scoresheet', function() {
 
-    it('can store scores', function() {
+    beforeEach(function() {
       scoresheet = new Scoresheet();
-      var frame = [1,2]
+    });
+
+    it('can store scores', function() {
+      var frame = [1,2];
       scoresheet.addFrame(frame);
       expect(scoresheet.board[0]).toBe(frame);
     });
 
     it('can store multiple scores', function() {
-      scoresheet = new Scoresheet();
       var frame_1 = [1,2];
       var frame_2 = [2,3];
       scoresheet.addFrame(frame_1);
@@ -22,7 +24,6 @@ var scoresheet;
     });
 
     it('can score a whole board', function() {
-      scoresheet = new Scoresheet();
       scoresheet.board = [[1,1], [1,1],
                           [1,1], [1,1],
                           [1,1], [1,1],
@@ -30,33 +31,30 @@ var scoresheet;
                           [1,1], [1,1,1]];
       scoresheet.scoreFrames();
       scoresheet.sum();
-      expect(scoresheet.total).toBe(21)
+      expect(scoresheet.total).toBe(21);
     });
 
     it('can score a single strike correctly', function() {
-      scoresheet = new Scoresheet();
       scoresheet.board = [[10,0], [1,1],
                           [1,1], [1,1],
                           [1,1], [1,1],
                           [1,1], [1,1],
                           [1,1], [1,1,1]];
       scoresheet.update();
-      expect(scoresheet.total).toBe(31)
+      expect(scoresheet.total).toBe(31);
     });
 
     it('can score a single spare correctly', function() {
-      scoresheet = new Scoresheet();
       scoresheet.board = [[1,9], [5,3],
                           [1,1], [1,1],
                           [1,1], [1,1],
                           [1,1], [1,1],
                           [1,1], [1,1,1]];
       scoresheet.update();
-      expect(scoresheet.total).toBe(40)
+      expect(scoresheet.total).toBe(40);
     });
 
     it('can score a two strikes correctly', function() {
-      scoresheet = new Scoresheet();
       scoresheet.board = [[10,0], [10,0],
                           [2,0], [1,0],
                           [1,0], [1,0],
@@ -65,11 +63,10 @@ var scoresheet;
       scoresheet.scoreFrames();
       scoresheet.strikeCalc();
       scoresheet.sum();
-      expect(scoresheet.total).toBe(43)
+      expect(scoresheet.total).toBe(43);
     });
 
     it('can score a three strikes correctly', function() {
-      scoresheet = new Scoresheet();
       scoresheet.board = [[10,0], [10,0],
                           [10,0], [2,0],
                           [1,0], [1,0],
@@ -78,11 +75,10 @@ var scoresheet;
       scoresheet.scoreFrames();
       scoresheet.strikeCalc();
       scoresheet.sum();
-      expect(scoresheet.total).toBe(72)
+      expect(scoresheet.total).toBe(72);
     });
 
     it('can score a four strikes correctly', function() {
-      scoresheet = new Scoresheet();
       scoresheet.board = [[10,0], [10,0],
                           [10,0], [10,0],
                           [2,0], [1,0],
@@ -91,11 +87,10 @@ var scoresheet;
       scoresheet.scoreFrames();
       scoresheet.strikeCalc();
       scoresheet.sum();
-      expect(scoresheet.total).toBe(101)
+      expect(scoresheet.total).toBe(101);
     });
 
     it('can score a strike spare combination correctly', function() {
-      scoresheet = new Scoresheet();
       scoresheet.board = [[10,0], [1,9],
                           [5,1], [1,0],
                           [1,0], [1,0],
@@ -104,11 +99,10 @@ var scoresheet;
       scoresheet.scoreFrames();
       scoresheet.strikeCalc();
       scoresheet.sum();
-      expect(scoresheet.total).toBe(48)
+      expect(scoresheet.total).toBe(48);
     });
 
     it('can score a perfect game correctly', function() {
-      scoresheet = new Scoresheet();
       scoresheet.board = [[10,0], [10,0],
                           [10,0], [10,0],
                           [10,0], [10,0],
@@ -117,23 +111,22 @@ var scoresheet;
       scoresheet.scoreFrames();
       scoresheet.strikeCalc();
       scoresheet.sum();
-      expect(scoresheet.total).toBe(300)
+      expect(scoresheet.total).toBe(300);
     });
 
     it('can score a running tally', function() {
-      scoresheet = new Scoresheet();
       var frame_1 = [10,0];
       var frame_2 = [2,3];
       scoresheet.addFrame(frame_1);
       scoresheet.scoreFrames();
       scoresheet.strikeCalc();
       scoresheet.sum();
-      expect(scoresheet.total).toBe(10)
+      expect(scoresheet.total).toBe(10);
       scoresheet.addFrame(frame_2);
       scoresheet.scoreFrames();
       scoresheet.strikeCalc();
       scoresheet.sum();
-      expect(scoresheet.total).toBe(20)
+      expect(scoresheet.total).toBe(20);
     });
 
   });
--- src/Bowling.js
+++ src/Bowling.js
@@ -5,16 +5,16 @@ var scoreFrame = function(frame) {
 };
 
 var isStrike = function(frame) {
-  return (frame[0] === 10)
+  return (frame[0] === 10);
 };
 
 var isSpare = function(frame) {
-  return ( (frame[0] + frame[1]) === 10 &&  frame[0] != 10 )
+  return ( (frame[0] + frame[1]) === 10 && frame[0] != 10 );
 };
 
 var Scoresheet = function() {
   this.board = [[0,0],[0,0],[0,0],[0,0],[0,0],
-                [0,0],[0,0],[0,0],[0,0],[0,0,0]]
+                [0,0],[0,0],[0,0],[0,0],[0,0,0]];
   this.tally = [];
   this.round = 0;
   this.total = 0;
@@ -27,37 +27,35 @@ Scoresheet.prototype.addFrame = function(frame) {
 
 Scoresheet.prototype.scoreFrames = function() {
   this.tally = this.board.map(function(frame) {
-      return scoreFrame(frame)
+      return scoreFrame(frame);
     });
 };
 
-  // this.normalFrames.forEach instead of while loop
 Scoresheet.prototype.strikeCalc = function() {
   var n = 0;
   while ( n < 8 ) {
 
     if (isStrike(this.board[n])) {
       if (isStrike(this.board[n + 1]))
-        { this.tally[n] = 20 + this.board[n + 2][0] } // two strikes
+        { this.tally[n] = 20 + this.board[n + 2][0]; }
       else
-        { this.tally[n] = 10 + this.tally[n + 1] }; // strike
-    };
+        { this.tally[n] = 10 + this.tally[n + 1]; }
+    }
 
-    if (isSpare(this.board[n])) { this.tally[n] = 10 + this.board[n + 1][0] };
+    if (isSpare(this.board[n])) { this.tally[n] = 10 + this.board[n + 1][0]; }
 
     n += 1;
-  };
+  }
 
-  //You could make a helper method for accessing frame rolls
   if (isStrike(this.board[8]))
-    { this.tally[8] = 10 + this.board[9][0] + this.board[9][1] };
+    { this.tally[8] = 10 + this.board[9][0] + this.board[9][1]; }
   if (isSpare(this.board[8]))
-    { this.tally[8] = 10 + this.board[9][0] };
+    { this.tally[8] = 10 + this.board[9][0]; }
 };
 
 Scoresheet.prototype.sum = function() {
   this.total = this.tally.reduce(function(roll, currentVal) {
-    return roll + currentVal
+    return roll + currentVal;
   });
 };
 
